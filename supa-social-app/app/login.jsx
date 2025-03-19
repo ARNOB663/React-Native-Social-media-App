d6345ee4b809c34a8d36acdef05eeb38acdac2b8
Aniruddha Dev Nath <113041453+ARNOB663@users.noreply.github.com>
2025-03-19 23:33:03 +0600

    icons installation

--- supa-social-app/app/login.jsx
+++ supa-social-app/app/login.jsx
@@ -1,14 +1,17 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Home from '../assets/icons/Home.jsx'
+import { theme } from '../constants/theme.js'
+import Icon from '../assets/icons/index.jsx'
 
 const login = () => {
   return (
     <View>
       <Text>login</Text>
+      <Icon name="home" color="red" />
     </View>
   )
 }
-
 export default login
 
 const styles = StyleSheet.create({})
--- supa-social-app/app/welcome.jsx
+++ supa-social-app/app/welcome.jsx
@@ -44,7 +44,6 @@ const Welcome = () => {
                             </Pressable>
                          </View>
                    </View>
-
       </View>
     </ScreenWrapper>
   )
